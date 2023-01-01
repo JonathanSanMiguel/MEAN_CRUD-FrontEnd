13a7a299914bdeaced006f265d9a87cc6ac6751b
Jonathan San Miguel <guerreroz80@outlook.es>
2023-01-01 13:47:57 -0600

    creacion del componente employee

---
+++ src/app/interfaces/employee.interface.ts
@@ -0,0 +1,6 @@
+interface EmployeeResponse{
+    name: string
+    position: string
+    office: string
+    salary: number
+}
--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+
+// import { EmployeeResponse } from '../interfaces/employee.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
+
+
+  private Api_Url = 'http://localhost:4000/rest'
+
+  Read(){
+    const Url = `${this.Api_Url}/employees`
+
+    // return this.http.get<EmployeeResponse>()
+
+  }
+
 }
